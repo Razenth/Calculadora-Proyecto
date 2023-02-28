25a81210628f0c9d48e66eace389590241e8a893
Santiago <santiago.gonzalezagudelo@gmail.com>
2023-02-28 13:04:30 -0500

    Version 1 Finished

--- main.js
+++ main.js
@@ -1,24 +1,62 @@
-let mostrar= document.getElementById('mostrar')
+let result= document.getElementById('mostrar')
 let n1=document.getElementById('n1')
-let operador=document.getElementById('operador')
-let n2=document-getElementById('n2')
-let uno=document.getElementById('uno')
-let dos=document.getElementById('dos')
-let tres=document.getElementById('tres')
-let cuatro=document.getElementById('cuatro')
-let cinco=document.getElementById('cinco')
-let seis=document.getElementById('seis')
-let siete=document.getElementById('siete')
-let ocho=document.getElementById('ocho')
-let nueve=document.getElementById('nueve')
-let cero=document.getElementById('cero')
-let mas=document.getElementById('mas')
-let menos=document.getElementById('menos')
-let por=document.getElementById('por')
-let divir=document.getElementById('divir')
-let off=document.getElementById('apagar')
-let igual=document.getElementById('igual')
+let simbolo=document.getElementById('simbolo')
+let n2=document.getElementById('n2')
+let uno=document.getElementById('btn_uno')
+let dos=document.getElementById('btn_dos')
+let tres=document.getElementById('btn_tres')
+let cuatro=document.getElementById('btn_cuatro')
+let cinco=document.getElementById('btn_cinco')
+let seis=document.getElementById('btn_seis')
+let siete=document.getElementById('btn_siete')
+let ocho=document.getElementById('btn_ocho')
+let nueve=document.getElementById('btn_nueve')
+let cero=document.getElementById('btn_cero')
+let mas=document.getElementById('btn_mas')
+let menos=document.getElementById('btn_menos')
+let por=document.getElementById('btn_por')
+let divir=document.getElementById('btn_divir')
+let off=document.getElementById('btn_apagar')
+let igual=document.getElementById('btn_igual')
+let resultado=''
 
-uno.addEventListener('click',=>{
-    
-})
+function mostrar(){
+    result.innerText=resultado
+}
+
+function operador(operando){
+    switch(operando){
+        case '+':
+            resultado=+n1.value + +n2.value
+            simbolo.innerText='+'
+            console.log(resultado)
+            break
+
+        case '-':
+            resultado=+n1.value - +n2.value
+            simbolo.innerText='-'
+            console.log(resultado)
+            break
+
+        case '*':
+            resultado=parseInt(n1.value) * parseInt(n2.value)
+            simbolo.innerText='*'
+            console.log(resultado)
+            break
+
+        case '/':
+            resultado=+n1.value / +n2.value
+            simbolo.innerText='/'
+            console.log(resultado)
+            break
+
+        case 'AC':
+            resultado=''
+            simbolo.innerHTML=''
+            n1.value=''
+            n2.value=''
+            mostrar()
+}
+}
+
+
