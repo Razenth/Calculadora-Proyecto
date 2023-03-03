098354a847b067df8970a02b1df7aa649f6b9610
Santi <santiago.gonzalezagudelo@gmail.com>
2023-03-02 22:31:28 -0500

    Version 3 Finished

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-// for (let i=0; i<=9; i++){
-//     document.getElementById('btn_'+ i).addEventListener('click',(evento)=>mostrar(evento.target));
-
-// }
-
-// function mostrar(tecla){
-//     document.getElementById('mostrar').innerHTML=
-//         document.getElementById('mostrar').innerHTML+tecla.value
-// }
-
-// function mostrar2(tecla){
-//     document.getElementById('mostrar').innerHTML=
-//         document.getElementById('mostrar').innerHTML+tecla
-// }
-
-// addEventListener('keypress', evento=>{
-//     if(!isNaN(evento.key)){
-//         mostrar2(evento.key)
-//     }
-// })
-
 
 mostrar=document.getElementById('mostrar')
 document.onkeydown=teclas
@@ -62,7 +41,13 @@ function igual(){
 }
 
 function borrar(){
-    
+    numeros=NumPantalla.length
+    NumPantalla=NumPantalla.substring(0,numeros-1)
+
+    if(NumPantalla==''){
+        NumPantalla='0'
+    }
+    mostrar.innerHTML=NumPantalla
 }
 
 function borrarTodo(){
@@ -111,165 +96,3 @@ function teclas (enEvento) {
         borrar()
     }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let result= document.getElementById('mostrar')
-// let n1=document.getElementById('n1')
-// let simbolo=document.getElementById('simbolo')
-// let n2=document.getElementById('n2')
-// let uno=document.getElementById('btn_uno')
-// let dos=document.getElementById('btn_dos')
-// let tres=document.getElementById('btn_tres')
-// let cuatro=document.getElementById('btn_cuatro')
-// let cinco=document.getElementById('btn_cinco')
-// let seis=document.getElementById('btn_seis')
-// let siete=document.getElementById('btn_siete')
-// let ocho=document.getElementById('btn_ocho')
-// let nueve=document.getElementById('btn_nueve')
-// let cero=document.getElementById('btn_cero')
-// let mas=document.getElementById('btn_mas')
-// let menos=document.getElementById('btn_menos')
-// let por=document.getElementById('btn_por')
-// let divir=document.getElementById('btn_divir')
-// let off=document.getElementById('btn_apagar')
-// let igual=document.getElementById('btn_igual')
-// let resultado=''
-// let valor1=0
-
-// function mostrar(){
-//     if(simbolo.innerHTML=='+'){
-//         resultado=parseInt(valor1) + parseInt(n1.value)
-//         result.innerText=resultado
-//         n1.value=''
-//         simbolo.innerText=''
-//     }
-
-//     else if(simbolo.innerHTML=='-'){
-//         resultado=parseInt(valor1) - parseInt(n1.value)
-//         result.innerText=resultado
-//         n1.value=''
-//         simbolo.innerText=''
-//     }
-
-//     else if(simbolo.innerHTML=='*'){
-//         resultado=parseInt(valor1) * parseInt(n1.value)
-//         result.innerText=resultado
-//         n1.value=''
-//         simbolo.innerText=''
-//     }
-
-//     else if(simbolo.innerHTML=='/'){
-//         resultado=parseInt(valor1) / parseInt(n1.value)
-//         result.innerText=resultado
-//         n1.value=''
-//         simbolo.innerText=''
-//     }
-
-//     else{
-//         n1.value=''
-//         simbolo.innerText=''
-//         result.innerHTML=''
-//     }
-
-// }
-
-// function operador(operando){
-//     switch(operando){
-
-//         case '+':
-//             valor1=0
-//             valor1+=n1.value
-//             simbolo.innerText='+'
-//             n1.value=''
-//             break;
-
-//         case '-':
-//             valor1=0
-//             valor1+=n1.value
-//             simbolo.innerText='-'
-//             n1.value=''
-//             break;
-
-//         case '*':
-//             valor1=0
-//             valor1+=n1.value
-//             simbolo.innerText='*'
-//             n1.value=''
-//             break;
-
-//         case '/':
-//             valor1=0
-//             valor1+=n1.value
-//             simbolo.innerText='/'
-//             n1.value=''
-//             break;
-
-//         case 'AC':
-//             resultado=''
-//             simbolo.innerHTML=''
-//             n1.value=''
-//             valor1=''
-//             mostrar()
-// }
-// }
